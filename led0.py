881ae989b05b274fa4e794a2f25e2433445e9854
Luke <lucas.macd@gmail.com>
2016-02-18 21:21:37 -0500

    Successful project - LED lit for 1 second

--- led0.py
+++ led0.py
@@ -1,10 +1,15 @@
+# Simple python script to turn an LED on for 1 second.
+# Author: Lucas Macdonald
+
+
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
 print ("LED on")
-GPIO.output(18,GPIO.HIGH)
+GPIO.output(17,GPIO.HIGH)
 time.sleep(1)
 print ("LED off")
-GPIO.output(18,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
+GPIO.cleanup()
